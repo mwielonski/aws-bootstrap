72e6c364b0f6ed87412a65d61a75b4d51fb6d875
Mike Wielonski <mwielonski@gmail.com>
2020-01-25 08:46:56 -0500

    Add codebuild / codedeploy spec, v2

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -2,27 +2,34 @@
 STACK_NAME=awsbootstrap 
 REGION=us-east-2 
 CLI_PROFILE=awsbootstrap
-
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
+AWS_ACCOUNT_ID=`aws2 sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ===========" 
-aws cloudformation deploy \
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws2 cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME-setup \ 
-  --template-file setup.yml \ 
-  --no-fail-on-empty-changeset \ 
-  --capabilities CAPABILITY_NAMED_IAM \ 
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --no-execute-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
      CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ===========" 
+echo -e "\n\n=========== Deploying main.yml ==========="
 aws2 cloudformation deploy \
 --region $REGION \
 --profile $CLI_PROFILE \
@@ -31,11 +38,16 @@ aws2 cloudformation deploy \
 --no-fail-on-empty-changeset \
 --capabilities CAPABILITY_NAMED_IAM \
 --parameter-overrides \
-EC2InstanceType=$EC2_INSTANCE_TYPE
+  EC2InstanceType=$EC2_INSTANCE_TYPE \
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws2 cloudformation list-exports \
      --profile awsbootstrap \
-     --query "Exports[?Name=='InstanceEndpoint'].Value" 
+     --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
